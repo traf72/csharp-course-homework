a447b4524439c0651da23d22a034b5a736a03936
Aleksei Trofimov <traf72@gmail.com>
2024-04-25 17:20:25 +0300

    Lesson 6

---
+++ Lesson6_1/myClass.cs
@@ -0,0 +1,14 @@
+﻿// Приложение не компилируется и не запускается. Исправьте ошибки.
+// Нарушены некоторые конвенции именования. Исправьте наименования.
+// Измените Default namespace на Lesson6, чтобы все последующие типы автоматически создавались в неймспейсе Lesson6.
+
+namespace lesson6_1
+{
+    internal class myClass
+    {
+        void run(string[] Args)
+        {
+            Console.WriteLine("Hello, my friend!");
+        }
+    }
+}
