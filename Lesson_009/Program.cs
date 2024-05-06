468b494797f43cb579920f342aa1b4428883e515
Aleksei Trofimov <traf72@gmail.com>
2024-05-06 22:11:50 +0300

    Lesson 9

---
+++ Lesson_009/Program.cs
@@ -0,0 +1,24 @@
+﻿// Какой фактический тип имеют данные неявно типизированные переменные
+
+var tempareture = 20.5f;
+var length = 758934u;
+var balance = 1_425_580.54M;
+var speed = 120;
+var bitFlags = 0b_1010_0001;
+var nan = double.NaN;
+var fileSizeInBytes = 74238075234;
+var roomsCount = (short)3;
+var massOfTheSun = 1.989e30;
+var distance = 758934LU;
+var positiveInfinity = float.PositiveInfinity;
+var memoryAddress = 0xF0423AC5F;
+var interestRate = 0.035;
+var electronCharge = -1.602e-19;
+var zero = decimal.Zero;
+
+// Исправьте ошибки компиляции
+float rate = 12.5;
+decimal cash = 525.3;
+byte grade = 5u;
+long distanceToAlphaCentauri = 4.37e16;
+int percentage = 80.0;
