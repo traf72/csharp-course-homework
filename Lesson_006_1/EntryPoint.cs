6099d33812c7dd84f3cc3cc05cb7cda3d1fc06d2
Aleksei Trofimov <traf72@gmail.com>
2024-09-08 20:31:32 +0300

    Lesson 6

---
+++ Lesson_006_1/EntryPoint.cs
@@ -0,0 +1,16 @@
+﻿/*
+    1. Приложение не компилируется и не запускается. Исправьте ошибки.
+    2. Нарушены некоторые конвенции именования. Исправьте наименования.
+    3. Измените Default namespace на Lesson6, чтобы все последующие типы автоматически создавались в неймспейсе Lesson6.
+*/
+
+namespace lesson6_1
+{
+    internal class entryPoint
+    {
+        void run(string[] Args)
+        {
+            Console.WriteLine("Hello, my friend!");
+        }
+    }
+}
